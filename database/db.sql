9b29d97365751ef1f4a81a50f4406c2b8275634e
WGreenwichChaca <walter.greenwich@gmail.com>
2024-05-03 22:38:56 -0500

    node and postgres practice

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email)
+    VALUES ('John Doe', 'john@gmail.com'),
+    ('Jane Doe', 'jane@gmail.com');
+
+SELECT * FROM users;
